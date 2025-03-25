0423298f66c032b6d1927d548169a3920ac7e6bd
SyahJehanWonopatih <syahjehan00@gmail.com>
2025-03-26 01:33:24 +0700

    menambahkan procedur insertionsort

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -1,31 +1,49 @@
 #include <iostream>
 using namespace std;
 
-int arr[20];
+int Arr[20];
 int n;
+
 void input()
 {
     while (true)
     {
-        cout << "masukkan jumlah data pada array : ";
+        cout << "masukkan jumlah data pada Array : ";
         cin >> n;
         if (n <=20)
         {
-            break;
+        break;
         }
-        else{
-            cout << "nArray yang anda masukkan maksimal 20 20 Elemen.\n";
-
+        else
+        {
+        cout << "\nArray yang anda masukkan maksimal 20 Elemen.\n";
         }
         cout << endl;
         cout << "=====================" << endl;
-        cout << "masukkan elemen array" << endl;
+        cout << "masukkan elemen Array" << endl;
         cout << "=====================" << endl;
 
         for (int i = 0; i < n; i++)
         {
             cout << "data ke-" << (i + 1) << ": ";
-            cin >> arr[i];
+            cin >> Arr[i];
         }
+    }
+}
+
+void insertionSort()
+{
+    int temp;
+    int j, i;
+    for (i = 1; i <= n - 1; i++)
+    {
+        temp = Arr(i);
+        j = i - 1;
+        while (j>= 0 && Arr[j] > temp)
+        {
+            Arr[j + 1] = Arr[j];
+            j--;
+        }
+        Arr[j + 1] = temp;
     }
 }
